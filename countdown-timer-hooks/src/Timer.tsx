186aeeded2ceac05e9cf6a32c8dc0c7b1e13952a
AyamotoKohei <koh14taka2@gmail.com>
2021-11-19 23:08:38 +0900

    add: Timerの実装

---
+++ countdown-timer-hooks/src/Timer.tsx
@@ -0,0 +1,37 @@
+import { VFC, useEffect, useState } from 'react';
+import { Button, Card, Icon, Statistic } from 'semantic-ui-react';
+import './Timer.css';
+
+const Timer: VFC<{ limit: number }> = ({ limit }) => {
+  const [timerLeft, setTimerLeft] = useState(limit);
+  const reset = (): void => setTimerLeft(limit);
+  const tick = (): void => setTimerLeft((t) => t - 1);
+
+  useEffect(() => {
+    const timerId = setInterval(tick, 1000);
+
+    return () => clearInterval(timerId);
+  }, []);
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(() => {
+    if (timerLeft === 0) setTimerLeft(limit);
+  });
+
+  return (
+    <Card>
+      <Statistic className="number-board">
+        <Statistic.Label>time</Statistic.Label>
+        <Statistic.Value>{timerLeft}</Statistic.Value>
+      </Statistic>
+      <Card.Content>
+        <Button color="red" fluid onClick={reset}>
+          <Icon name="redo" />
+          reset
+        </Button>
+      </Card.Content>
+    </Card>
+  );
+};
+
+export default Timer;
