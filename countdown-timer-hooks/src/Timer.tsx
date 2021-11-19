2f34ba4edc01846bb1df2c1d393a9ec6606bcf74
AyamotoKohei <koh14taka2@gmail.com>
2021-11-19 23:22:39 +0900

    fix: デザインの修正

--- countdown-timer-hooks/src/Timer.tsx
+++ countdown-timer-hooks/src/Timer.tsx
@@ -3,9 +3,9 @@ import { Button, Card, Icon, Statistic } from 'semantic-ui-react';
 import './Timer.css';
 
 const Timer: VFC<{ limit: number }> = ({ limit }) => {
-  const [timerLeft, setTimerLeft] = useState(limit);
-  const reset = (): void => setTimerLeft(limit);
-  const tick = (): void => setTimerLeft((t) => t - 1);
+  const [timeLeft, setTimeLeft] = useState(limit);
+  const reset = (): void => setTimeLeft(limit);
+  const tick = (): void => setTimeLeft((t) => t - 1);
 
   useEffect(() => {
     const timerId = setInterval(tick, 1000);
@@ -14,19 +14,19 @@ const Timer: VFC<{ limit: number }> = ({ limit }) => {
   }, []);
 
   useEffect(() => {
-    if (timerLeft === 0) setTimerLeft(limit);
-  }, [timerLeft, limit]);
+    if (timeLeft === 0) setTimeLeft(limit);
+  }, [timeLeft, limit]);
 
   return (
     <Card>
       <Statistic className="number-board">
         <Statistic.Label>time</Statistic.Label>
-        <Statistic.Value>{timerLeft}</Statistic.Value>
+        <Statistic.Value>{timeLeft}</Statistic.Value>
       </Statistic>
       <Card.Content>
         <Button color="red" fluid onClick={reset}>
           <Icon name="redo" />
-          reset
+          Reset
         </Button>
       </Card.Content>
     </Card>
